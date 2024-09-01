e09bf0d5022c2e8dd6c84c9c4f59306b40476fab
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-09-01 08:26:55 +0200

    Create seperate tests for compression methods

--- FAForever.Replay.Test/ReplayLoaderTest.cs
+++ FAForever.Replay.Test/ReplayLoaderTest.cs
@@ -4,41 +4,34 @@ namespace FAForever.Replay.Test;
 public class ReplayLoaderTest
 {
     [TestMethod]
-    [DataRow("assets/22338092.scfareplay")]
-    [DataRow("assets/22373098.scfareplay")]
-    [DataRow("assets/22425616.scfareplay")]
-    public void TestMethod1(string scfaReplayFile)
+    [DataRow("assets/faforever/zstd/22338092.fafreplay")]
+    [DataRow("assets/faforever/zstd/22373098.fafreplay")]
+    [DataRow("assets/faforever/zstd/22425616.fafreplay")]
+    public void FAForeverZSTDTest(string file)
     {
-
-        using (FileStream stream = new FileStream("assets/scfa/22338092.scfareplay", FileMode.Open))
-        {
-            ReplayBinaryReader reader = new ReplayBinaryReader(stream);
-            Replay replay = ReplayLoader.ParseReplay(reader);
-            Assert.IsNotNull(replay);
-            Assert.IsNotNull(replay.Header);
-            Assert.IsNotNull(replay.Events);
-        }
-
-
-        using (FileStream stream = new FileStream("assets/faforever/22338092.fafreplay", FileMode.Open))
-        {
-            Replay replay = ReplayLoader.ParseFAFReplayFromMemory(stream);
-            Assert.IsNotNull(replay);
-            Assert.IsNotNull(replay.Header);
-            Assert.IsNotNull(replay.Events);
-        }
-
+        Replay replay = ReplayLoader.LoadFAFReplayFromDisk(file);
+        Assert.IsNotNull(replay);
     }
 
     [TestMethod]
-    [DataRow("assets/faforever/22338092.fafreplay", 15)]
-    [DataRow("assets/faforever/22373098.fafreplay", 185)]
-    [DataRow("assets/faforever/22425616.fafreplay", 104)]
-    [DataRow("assets/faforever/22451957.fafreplay", 14)]
-    [DataRow("assets/faforever/22453414.fafreplay", 29)]
-    [DataRow("assets/faforever/22453511.fafreplay", 25)]
+    [DataRow("assets/faforever/gzip/22451957.fafreplay")]
+    [DataRow("assets/faforever/gzip/22453414.fafreplay")]
+    [DataRow("assets/faforever/gzip/22453511.fafreplay")]
+    public void FAForeverGZipTest(string file)
+    {
+        Replay replay = ReplayLoader.LoadFAFReplayFromDisk(file);
+        Assert.IsNotNull(replay);
+    }
+
+    [TestMethod]
+    [DataRow("assets/faforever/zstd/22338092.fafreplay", 15)]
+    [DataRow("assets/faforever/zstd/22373098.fafreplay", 185)]
+    [DataRow("assets/faforever/zstd/22425616.fafreplay", 104)]
+    [DataRow("assets/faforever/gzip/22451957.fafreplay", 14)]
+    [DataRow("assets/faforever/gzip/22453414.fafreplay", 29)]
+    [DataRow("assets/faforever/gzip/22453511.fafreplay", 25)]
     [DataRow("assets/faforever/TestCommands01.fafreplay", 15640)]
-    public void EventCountFAForeverTest(string file, int expectedCount)
+    public void UserInputCountTest(string file, int expectedCount)
     {
         Replay replay = ReplayLoader.LoadFAFReplayFromDisk(file);
         Assert.AreEqual(replay.Events.Count, expectedCount);
