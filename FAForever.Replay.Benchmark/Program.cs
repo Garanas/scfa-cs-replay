467fc1ce98228a2bca1e8770f970d5df7c5c4fc7
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-09-01 08:29:01 +0200

    Introduce a benchmark orientated project

---
+++ FAForever.Replay.Benchmark/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FAForever.Replay.Benchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
