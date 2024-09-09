5b8d2f8a771a8795ab4be617a5c03686e13fd94d
(Jip) Willem Wijnia <applicaties@jipwijnia.nl>
2024-09-09 20:33:18 +0200

    Draft setup for building blocks of a Blazor app

---
+++ FAForever.Replay.Viewer/Services/ReplayService.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Reactive.Subjects;
+
+namespace FAForever.Replay.Viewer.Services
+{
+    public class ReplayService
+    {
+        
+        public Replay? Replay { get; set; }
+
+        public void LoadReplay(MemoryStream stream)
+        {
+            this.Replay = ReplayLoader.LoadFAFReplayFromMemory(stream);
+        }
+    }
+}
